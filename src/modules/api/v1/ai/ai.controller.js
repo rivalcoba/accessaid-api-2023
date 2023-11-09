cc0fe7a57b0ad402ae17b77e48992a4729a2d4be
rivalcoba <ivan.rivalcoba@gmail.com>
2023-11-08 22:12:27 -0600

    ⭐ Feat: Mirror Body

--- src/modules/api/v1/ai/ai.controller.js
+++ src/modules/api/v1/ai/ai.controller.js
@@ -1,10 +1,11 @@
 import httpStatus from 'http-status';
 
 // GET /api/v1/users
-export function aiRoot(_, res) {
+export function aiRoot(req, res) {
   res.status(httpStatus.OK).json({
     result: 'ok',
     message: 'AI Test Passed!!',
+    body: req.body,
   });
 }
 
