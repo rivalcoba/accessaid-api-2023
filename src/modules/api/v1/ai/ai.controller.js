88d39b79e2ab084d46dc0c4da486434ff9d76e71
rivalcoba <ivan.rivalcoba@gmail.com>
2023-11-09 01:04:10 -0600

    ⭐ Feat: Improve Json

--- src/modules/api/v1/ai/ai.controller.js
+++ src/modules/api/v1/ai/ai.controller.js
@@ -1,5 +1,6 @@
 import httpStatus from 'http-status';
 import OpenAI from 'openai';
+import log from '../../../../config/winston';
 
 const openai = new OpenAI({
   organization: 'org-4Cmf2LvEbFLEHoq9Fl8qIcox',
@@ -14,10 +15,19 @@ export function aiRoot(req, res) {
 // POST /api/v1/ai/test
 export async function aiTest(req, res) {
   const completion = await openai.chat.completions.create({
-    messages: [{ role: 'user', content: 'Tell me a joke!' }],
-    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'user',
+        content: `
+        Haz una lista de 5 numeros en romano en formato json, 
+        la clave es el romano y el valor su equivalente numérico 
+        `,
+      },
+    ],
+    model: 'gpt-3.5-turbo-1106', // gpt-3.5-turbo-1106 gpt-3.5-turbo
+    response_format: { type: 'json_object' },
   });
-  console.log(completion.choices[0].message.content);
+  log.info(completion.choices[0].message.content);
   res.status(httpStatus.OK).json(completion.choices[0].message);
 }
 
